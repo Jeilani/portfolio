0b3021df04bad4471b8f95f57ec9e5ae3ffc2dc8
Jeilani Mohamed <Jeilani@Jeilanis-MacBook-Pro.local>
2020-01-30 15:17:49 -0700

    Updates

---
+++ precache-manifest.ff8f7497e05ada15b4a06c0324eb7735.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12173de33fb6b326f857a727f30e0e1",
+    "url": "/Updated-Portfolio-2020/index.html"
+  },
+  {
+    "revision": "c552af93c419e3cff860",
+    "url": "/Updated-Portfolio-2020/static/css/main.084f1a13.chunk.css"
+  },
+  {
+    "revision": "ac4215ec8777ea16ccb5",
+    "url": "/Updated-Portfolio-2020/static/js/2.353c10f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Updated-Portfolio-2020/static/js/2.353c10f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c552af93c419e3cff860",
+    "url": "/Updated-Portfolio-2020/static/js/main.4c63a6ca.chunk.js"
+  },
+  {
+    "revision": "0921a90fbb9e6e533999",
+    "url": "/Updated-Portfolio-2020/static/js/runtime-main.4b3317d4.js"
+  },
+  {
+    "revision": "29401c6d0d56d239c6f4c50c1b968b26",
+    "url": "/Updated-Portfolio-2020/static/media/OldPortfolioThumbnail.29401c6d.png"
+  },
+  {
+    "revision": "251168732f6b930d376befce75e6baf9",
+    "url": "/Updated-Portfolio-2020/static/media/OrganizerScreenshot.25116873.png"
+  },
+  {
+    "revision": "3b8b694e40c4c0092f8eab2c3a946da1",
+    "url": "/Updated-Portfolio-2020/static/media/PatatapThumbnail.3b8b694e.png"
+  },
+  {
+    "revision": "c0358060c01cdb28545d58c0c5f76971",
+    "url": "/Updated-Portfolio-2020/static/media/TodoAPI.c0358060.png"
+  },
+  {
+    "revision": "0417f22b44b4256d13424cedad291e83",
+    "url": "/Updated-Portfolio-2020/static/media/YelpCampScreenshot.0417f22b.png"
+  },
+  {
+    "revision": "9a556e31936b83ff3bebfbca9acc01c4",
+    "url": "/Updated-Portfolio-2020/static/media/airjordanthumbnail.9a556e31.png"
+  },
+  {
+    "revision": "5af0fbad28f51a9b353a1e148a04ad2c",
+    "url": "/Updated-Portfolio-2020/static/media/asteroidthumbnail.5af0fbad.png"
+  },
+  {
+    "revision": "d297a9d531bd041594417ac517059c6e",
+    "url": "/Updated-Portfolio-2020/static/media/randomgeneratorthumbnail.d297a9d5.png"
+  },
+  {
+    "revision": "868ebc8905931f15aef65a923bf28137",
+    "url": "/Updated-Portfolio-2020/static/media/todolistthumbnail.868ebc89.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.725e3012d307f5ad6eebed0ce8636fe3.js"
+  "/Updated-Portfolio-2020/precache-manifest.ff8f7497e05ada15b4a06c0324eb7735.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Updated-Portfolio-2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
